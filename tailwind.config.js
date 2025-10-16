69919f50158b575f55222abbab262645af35bbc2
IMAMSB6409 <imam0406@student.abudzar.sch.id>
2025-10-16 09:17:00 +0700

    update navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
- /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-   content: ["./src/**/*.{html,js}", "./index.html"],
-   theme: {
-     extend: {},
-   },
-   plugins: [],
- }
+  content: ["./src/**/*.{html,js}", "./index.html"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        "bgPrimary": "#2A6F97",
+      },
+    },
+  },
+  plugins: [],
+};
